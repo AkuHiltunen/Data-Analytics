cba266603bcee5cd7bf081b86dbdcde9ee2684ae
Aku Hiltunen <aku.hiltunen91@gmail.com>
2018-04-01 19:24:20 +0300

    Updated the script so it writes three separate files with unique information

--- twitter-parse-topics-mentions-polarity.py
+++ twitter-parse-topics-mentions-polarity.py
@@ -1,13 +1,17 @@
+#Aku Hiltunen, Tampere University of Technology
+#script for parsing tweets, mentions and polarity out of a csv file created with TAGS
+
 import re
 from textblob import TextBlob
 
 import pandas as pd
 from ttp import ttp as prep
 
-
+#function for cleaning a tweet for sentiment analysis
 def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
 
+#function for performing the sentiment analysis
 def get_tweet_sentiment(tweet):
     '''
     Utility function to classify sentiment of passed tweet
@@ -23,46 +27,98 @@ def get_tweet_sentiment(tweet):
     else:
         return 'negative'
 
+#sets up filepath for an incoming csv file, change path to fit your case
 filename = "../twitterdata/samsung.csv"
 
+#reads a csv file into the program
 df = pd.read_csv(filename, sep=',', encoding="utf-8")
+
+#prints are used for testing purposes throughout the script
 print(df.head())
 parser = prep.Parser()
 
 print(df.columns)
 
-#mentions = []
-#hashtags = []
+#sets up new dicts to store mentions and hashtags
+mentions = {'user': [], 'times mentioned': [], 'positivity': [], 'negativity': [], 'neutrality': []}
+hashtags = {'hashtag': [], 'times used': [], 'positivity': [], 'negativity': [], 'neutrality': []}
 
-mention_columns = [[], [], [], [], [], [], [], [], [], []]
-hashtag_columns = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-                   [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
-print(mention_columns)
+#sets up a list for the polarity column
 polarities = []
 
+#goest through the table one row at a time
 for row in df.itertuples():
   tweet = parser.parse(row.text.encode('utf-8').decode('utf-8'))
   user_from = row.from_user.lower()
+  sentiment = get_tweet_sentiment(row.text)
 
-  count = 0
-
+  #this for loop collects information about mentions and stores it into the mentions dict created earlier
   for mention in tweet.users:
-      mention_columns[count].append(mention)
-      count += 1
+      ignore = 0
+      user_to = mention.lower()
 
-  while count < 10:
-      mention_columns[count].append('')
-      count += 1
+      for index, user in enumerate(mentions['user']):
+          if user_to == user:
+              mentions['times mentioned'][index] += 1
+              if sentiment == 'positive':
+                  mentions['positivity'][index] += 1
+              elif sentiment == 'negative':
+                  mentions['negativity'][index] += 1
+              else:
+                  mentions['neutrality'][index] += 1
+              ignore += 1
+              break
 
-  count = 0
+      if ignore == 0:
+        mentions['user'].append(user_to)
+        mentions['times mentioned'].append(1)
+        if sentiment == 'positive':
+            mentions['positivity'].append(1)
+            mentions['negativity'].append(0)
+            mentions['neutrality'].append(0)
+        elif sentiment == 'negative':
+            mentions['positivity'].append(0)
+            mentions['negativity'].append(1)
+            mentions['neutrality'].append(0)
+        else:
+            mentions['positivity'].append(0)
+            mentions['negativity'].append(0)
+            mentions['neutrality'].append(1)
 
+  #this for loop collects information about hashtags and stores it into the hashtags dict created earlier
   for tag in tweet.tags:
-      hashtag_columns[count].append(tag)
-      count += 1
+      ignore = 0
+      hashtag = tag.lower()
+      for index, thing in enumerate(hashtags['hashtag']):
+          if hashtag == thing:
+              hashtags['times used'][index] += 1
+              if sentiment == 'positive':
+                  hashtags['positivity'][index] += 1
+              elif sentiment == 'negative':
+                  hashtags['negativity'][index] += 1
+              else:
+                  hashtags['neutrality'][index] += 1
+              ignore += 1
+              break
 
-  while count < 10:
-      hashtag_columns[count].append('')
-      count += 1
+      if ignore == 0:
+          hashtags['hashtag'].append(hashtag)
+          hashtags['times used'].append(1)
+          if sentiment == 'positive':
+              hashtags['positivity'].append(1)
+              hashtags['negativity'].append(0)
+              hashtags['neutrality'].append(0)
+          elif sentiment == 'negative':
+              hashtags['positivity'].append(0)
+              hashtags['negativity'].append(1)
+              hashtags['neutrality'].append(0)
+          else:
+              hashtags['positivity'].append(0)
+              hashtags['negativity'].append(0)
+              hashtags['neutrality'].append(1)
+
+  #stores the polarity for each row
+  polarities.append(sentiment)
 
   #part of old implementation that lumps all tags and mentions in a single column
   '''mention_list = []
@@ -79,48 +135,35 @@ for row in df.itertuples():
   #print(tweet.tags)
   hashtags.append(tag_list)'''
 
-  sentiment = get_tweet_sentiment(row.text)
-  polarities.append(sentiment)
-
+#old implementation
 #df['mentions'] = mentions
 #df['hashtags'] = hashtags
 #print(hashtags)
 
-df['mention 1'] = mention_columns[0]
-df['mention 2'] = mention_columns[1]
-df['mention 3'] = mention_columns[2]
-df['mention 4'] = mention_columns[3]
-df['mention 5'] = mention_columns[4]
-df['mention 6'] = mention_columns[5]
-df['mention 7'] = mention_columns[6]
-df['mention 8'] = mention_columns[7]
-df['mention 9'] = mention_columns[8]
-df['mention 10'] = mention_columns[9]
-
-df['hashtag 1'] = hashtag_columns[0]
-df['hashtag 2'] = hashtag_columns[1]
-df['hashtag 3'] = hashtag_columns[2]
-df['hashtag 4'] = hashtag_columns[3]
-df['hashtag 5'] = hashtag_columns[4]
-df['hashtag 6'] = hashtag_columns[5]
-df['hashtag 7'] = hashtag_columns[6]
-df['hashtag 8'] = hashtag_columns[7]
-df['hashtag 9'] = hashtag_columns[8]
-df['hashtag 10'] = hashtag_columns[9]
-
+#creates a new column for polarity
 df['polarity'] = polarities
 
-df2 = df['polarity']
-
 print(df.head())
+print(mentions['user'])
+print(mentions['times mentioned'])
 
+df2 = pd.DataFrame(data=mentions)
+df3 = pd.DataFrame(data=hashtags)
+
+#sets up filepaths for the csv files to be written
 output = "../twitterdata/output.csv"
+output2 = "../twitterdata/mentions.csv"
+output3 = "../twitterdata/hashtags.csv"
+
+#writes three new csv files
+#first one is the original + a new column for polarity
+
+#second one includes columns for each unique mention, number of times a mention was mentioned +
+#number of times a mention appeared in the context of each polarity
+
+#third one includes columns for each unique hashtag, number of times a hashtag was used +
+#number of times a hashtag appeared in the context of each polarity
 
 df.to_csv(output, sep=',', encoding='utf-8')
-
-#can write columns to separate files
-#output2 = "../twitterdata/output2.csv"
-#df2.to_csv(output2, sep=',', encoding='utf-8')
-
-
-
+df2.to_csv(output2, sep=',', encoding='utf-8')
+df3.to_csv(output3, sep=',', encoding='utf-8')
