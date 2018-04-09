cebbf57292b269139134bdd6e4cadfec1d87d141
Aku Hiltunen <aku.hiltunen91@gmail.com>
2018-04-09 15:51:01 +0300

    added some comments to the network creating script

--- twitter-create-network.py
+++ twitter-create-network.py
@@ -1,6 +1,11 @@
+#a script for creating a graph file out of a csv file output from TAGS
+#graph files are used in Gephi
 import pandas as pd
 from ttp import ttp as prep
 import networkx as nx
+
+#enter filepath for your data
+filename = "../twitterdata/samsung.csv"
 
 df = pd.read_csv(filename, sep=',', encoding="utf-8")
 print(df.head())
@@ -15,6 +20,8 @@ for row in df.itertuples():
   user_from = row.from_user.lower()
   #print(tweet.users)
   new_list = []
+
+  #this for loop checks for a bug that is found in the imported parsing function
   for i in tweet.users:
     if i[-1] == "c" and i[-2] == "i" and i[-3] == "p":
       print(i)
@@ -30,7 +37,8 @@ for row in df.itertuples():
         network.add_edge(user_from, user_to, weight=0)
       network[user_from][user_to]['weight'] += 1
 
+#writes the graph file
 nx.readwrite.gexf.write_gexf(network,
-  'network.gexf',
+  '../twitterdata/network.gexf',
   encoding='utf-8', version='1.2draft')
 
